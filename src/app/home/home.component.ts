926ddeb98bb40a79c423d97162f17c2b69139b5e
cameronmcguigan <cmcguigan@actum.de>
2023-10-11 13:16:50 +0200

    Connection with BE

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Router } from "@angular/router";
 import { UserService } from "../user/user.service";
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -14,10 +12,12 @@ export class HomeComponent {
   isOverAge = false;
   selectedFile: File | null = null;
   private url = "http://localhost:8080/upload"
-  private username = localStorage.getItem("User");
-  private headers = new HttpHeaders({'username': this.username!});
+  private email = localStorage.getItem("email");
 
-  constructor(private http: HttpClient, private router: Router, private userService: UserService, private snackBar: MatSnackBar) {
+  constructor(private http: HttpClient, private router: Router, private userService: UserService) {
+    if (!userService.isLoggenIn()){
+      router.navigate(['/login']);
+    }
     this.checkIsOverAge();
   }
 
@@ -36,23 +36,13 @@ export class HomeComponent {
     if (this.selectedFile) {
       const formData = new FormData();
       formData.append('file', this.selectedFile);
+      formData.append('email', this.email!)
 
-      this.http.post(this.url, formData, { headers: this.headers }).subscribe(
+      this.http.post(this.url, formData).subscribe(
           response => {
-            // Show a success message
-            this.showSnackBar('File upload successful', 'success');
             console.log('File upload success:', response);
           }
       );
     }
-  }
-
-
-  showSnackBar(message: string, panelClass: string) {
-    const config = new MatSnackBarConfig();
-    config.panelClass = [panelClass];
-    config.duration = 3000; // Duration in milliseconds (adjust as needed)
-
-    this.snackBar.open(message, 'Close', config);
   }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -7,27 +7,50 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = "http://localhost:8080/user"; //tbd
-  private username = localStorage.getItem("User");
-  private headers = new HttpHeaders({'username': this.username!});
+  private apiUrl = "http://localhost:8080";
+  private email: any;
   constructor(private http:HttpClient, private router: Router) { }
 
-    loginUser(credentials: ɵTypedOrUntyped<any, ɵFormGroupValue<any>, any>) {
-      const url = this.apiUrl + "/login";
-      return this.http.post<any>(url, credentials, {observe: 'response'}).subscribe(
-        response => {
-          /*const user = response.headers.get("User")
-          localStorage.setItem("User", user);
-          this.router.navigate(['/home']);*/
+    loginUser(credentials: any) {
+        const url = this.apiUrl + "/login";
+
+        this.http.post<any>(url, credentials).subscribe(response => {
+            console.log(response);
+
+            if (response && response.email) {  // Assuming 'email' is the property returned in the response.
+                // Save email to local storage
+                localStorage.setItem("email", response.email);
+                this.email = response.email;
+                this.router.navigate(['/home']);
+            }
+        }, error => {
+            console.error('Login failed:', error);
         });
     }
 
     registerUser(value: ɵTypedOrUntyped<any, ɵFormGroupValue<any>, any>) {
+      const url = this.apiUrl + "/register";
+      return this.http.post<any>(url, value).subscribe(
+          response => {
+              console.log(response);
 
+              if (response && response.email) {  // Assuming 'email' is the property returned in the response.
+                  // Save email to local storage
+                  localStorage.setItem("email", response.email);
+                  this.email = response.email;
+                  this.router.navigate(['/home']);
+              }
+          }, error => {
+              console.error('Register failed:', error);
+          });
     }
 
     getUserAge() {
       const url = this.apiUrl + "/age";
-      return this.http.get(url,{headers: this.headers});
+      return this.http.post(url, this.email);
+    }
+
+    isLoggenIn() {
+        return this.email != null;
     }
 }
